d584734f58fe7c2563a773ee14d3bef3e567883f
lakshay <guptalakshay90@gmail.com>
2020-09-01 02:24:56 +0530

    Indented the code

--- src/reducers/trend.js
+++ src/reducers/trend.js
@@ -1,50 +1,55 @@
 const initialState = {
-    result:[[]],
-    sending:false,
-    keyArr:[],
-    dailyTrending:false
-  };
+    result: [
+        []
+    ],
+    sending: false,
+    keyArr: [],
+    dailyTrending: false
+};
 
-  export default function (state = initialState, action) {
-    const { type, payload } = action;
+export default function(state = initialState, action) {
+    const {
+        type,
+        payload
+    } = action;
     switch (type) {
-    
+
         case 'SENDING':
-            return{
+            return {
                 ...state,
                 sending: true
             }
-        case 'TREND_RECEIVED':
-            return {
-                ...state,
-                sending:false,
-                result:payload
-            }
+            case 'TREND_RECEIVED':
+                return {
+                    ...state,
+                    sending: false,
+                        result: payload
+                }
 
-        case 'TREND_ERROR':
-            return {
-                ...state,
-                sending:false
-            }
-            
-            case 'TRENDING':
-                return{
-                    ...state,
-                    dailyTrending: true
-                }
-            case 'TRENDING_RECEIVED':
-                return {
-                    ...state,
-                    dailyTrending:false,
-                   keyArr:payload
-                }
-    
-            case 'TRENDING_ERROR':
-                return {
-                    ...state,
-                    dailyTrending:false
-                }    
-        default:   
-        return state 
+                case 'TREND_ERROR':
+                    return {
+                        ...state,
+                        sending: false
+                    }
+
+                    case 'TRENDING':
+                        return {
+                            ...state,
+                            dailyTrending: true
+                        }
+                        case 'TRENDING_RECEIVED':
+                            return {
+                                ...state,
+                                dailyTrending: false,
+                                    keyArr: payload
+                            }
+
+                            case 'TRENDING_ERROR':
+                                return {
+                                    ...state,
+                                    dailyTrending: false
+                                }
+                                default:
+                                    return state
     }
 }
